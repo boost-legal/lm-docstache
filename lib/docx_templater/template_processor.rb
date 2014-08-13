b45a7f762e5472b650313a42d42210a9f2e8518e
Florent Bouron <florent@intuisec.com>
2014-08-13 17:54:46 +1200

    modified:   template_processor.rb

--- lib/docx_templater/template_processor.rb
+++ lib/docx_templater/template_processor.rb
@@ -2,32 +2,95 @@ require 'nokogiri'
 
 module DocxTemplater
   class TemplateProcessor
-    attr_reader :data, :escape_html
+    attr_reader :data, :document, :escape_html
 
     # data is expected to be a hash of symbols => string or arrays of hashes.
-    def initialize(data, escape_html = true)
+    def initialize(data, document, escape_html = true)
       @data = data
+      @xml = Nokogiri::XML(document)
       @escape_html = escape_html
     end
 
-    def render(document)
+    def render
       document.force_encoding(Encoding::UTF_8) if document.respond_to?(:force_encoding)
-      xml = Nokogiri::XML(document)
-      # Lookup replacement to perform
-      key_struct = "^\$[.*]\$$"
-      xml.xpath("//w:tr[contains(., key_struct)]", xml.root.namespaces)
-      data.each do |key, value|
-        if value.class == Array
-          document = enter_multiple_values(document, key)
-          document.gsub!("#SUM:#{key.to_s.upcase}#", value.count.to_s)
-        else
-          document.gsub!("$#{key.to_s.upcase}$", safe(value))
-        end
-      end
-      document
+
+      expanded_xml = expand_document(@xml, data)
+      out_xml = replace_data(expanded_xml, data)
+
+      return out_xml
     end
 
     private
+
+    def expand_document()
+
+      rows = @xml.xpath('//w:tr', @xml.root.namespaces)
+      expanded = process_rows(rows)
+
+      cells = @xml.xpath('//w:t', @xml.root.namespaces)
+      out_xml = process_cells(cells)
+      return @xml
+    end
+
+    def process_rows(rows)
+      rows.each do |row|
+
+        puts "Row Text: #{row.text}"
+        # Process Row
+        if !/BEGIN_ROW:/.match(row.text).nil?
+          loop_start = row
+          /#BEGIN_ROW:(?<key>[[:upper:]]+)#/ =~ row.text
+          loop_end = @xml.xpath("//w:tr[contains(., '#END_ROW:#{key.to_s.upcase}#')]", @xml.root.namespaces).first
+
+          loop_content = create_loop_list(loop_start,loop_end)
+          process_loop(loop_content)
+        elsif !/END_ROW/.match(row.text).nil?
+          puts "skip and out of loop: #{row.text}"
+          return row.next_sibling
+        else
+          @begin.add_next_sibling(replace_row(@row))
+          puts "replace: #{row.text}"
+          rows_for_key = replace_row(row, key)
+        end
+      end
+    end
+
+    def create_loop_list(loop_start, loop_end)
+      out = []
+      row = loop_start.next_sibling
+      until row.next_sibling == loop_end
+        out.append(row)
+        row = row.next_sibling
+      end
+      return out
+    end
+
+    def process_loop(row)
+    end
+
+    def replace_row(row, loop_key)
+      @out_rows = Array.new
+      @elements = @data[key.downcase]
+      innards = row.inner_html
+      matches = innards.scan(/\$EACH:([^\$]+)\$/)
+      unless matches.empty?
+        @elements.each do |element|
+          if element.keys.map(&:to_sym) == matches.map(&:first).map(&:downcase).map(&:to_sym)
+            new_row = row.dup
+
+            matches.map(&:first).each do |each_key|
+              new_inner_html = new_row.inner_html.gsub!("$EACH:#{each_key}$", element[each_key.downcase.to_sym]))
+              new_row.inner_html = new_inner_html
+            end
+
+            @out_rows.append(new_row)
+          end
+        end
+      end
+
+      new_row.inner_html = innards
+      return new_row
+    end
 
     def safe(text)
       if escape_html
@@ -35,10 +98,6 @@ module DocxTemplater
       else
         text.to_s
       end
-    end
-
-    def dict
-      ['TAG', 'EACH', 'BEGIN_ROW', 'END_ROW']
     end
 
     def parse_row(xml, key, values)
