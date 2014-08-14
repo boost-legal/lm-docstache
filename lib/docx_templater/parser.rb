6f092dd3ae390aac438d00e7d59c78130439af88
Florent Bouron <florent@intuisec.com>
2014-08-14 23:02:48 +1200

    fully operational

--- lib/docx_templater/parser.rb
+++ lib/docx_templater/parser.rb
@@ -33,22 +33,40 @@ module DocxTemplater
       @zipfile = Zip::File.new(@in_filepath)
     end
     
-    def render
-      @garbage = Array.new
+    def render_file
       document_xml = unzip_read("word/document.xml")
       footnotes_xml = unzip_read("word/footnotes.xml")
       
-      @content = Nokogiri::XML(document_xml)
-      @footnotes = Nokogiri::XML(footnotes_xml)
+      content = Nokogiri::XML(document_xml)
+      footnotes = Nokogiri::XML(footnotes_xml)
      
-      @garbage += parse_content(@content.elements)
-      @garbage += parse_content(@footnotes.elements)
-
-      @garbage.map(&:unlink)
-     
-      zip_write(@out_filepath, @content, @footnotes) 
-    end
+      parse_content(content.elements)
+      parse_content(footnotes.elements)
     
+      buffer = zip_create(content, footnotes)
+      if File.open(@out_filepath, "w") {|f| f.write(buffer.string) }
+        return @out_filepath.to_s
+      else
+        return false
+      end
+    end
+
+    def render_stream
+      @garbage = Array.new
+      document_xml = unzip_read("word/document.xml")
+      footnotes_xml = unzip_read("word/footnotes.xml")
+
+      content = Nokogiri::XML(document_xml)
+      footnotes = Nokogiri::XML(footnotes_xml)
+
+      parse_content(content.elements)
+      parse_content(footnotes.elements)
+
+      buffer = zip_create(content, footnotes)
+      buffer.rewind
+      return buffer.sysread
+    end
+ 
     private
     
     def unzip_read(zip_path)
@@ -60,7 +78,7 @@ module DocxTemplater
       return contents
     end
 
-    def zip_write(zip_path, content, footnotes)
+    def zip_create(content, footnotes)
       buffer = Zip::OutputStream.write_buffer do |out|
         @zipfile.entries.each do |e|
           unless ['word/document.xml', 'word/footnotes.xml'].include?(e.name)
@@ -76,21 +94,22 @@ module DocxTemplater
         out.write footnotes.to_xml(:indent => 0).gsub("\n","")
       end
 
-      File.open(zip_path, "w") {|f| f.write(buffer.string) }
+      return buffer
     end
 
  
     def expand_loop(nd, key, data)
       garbage = Array.new
       if !data.has_key?(key)
-        return []
+        nil
       elsif data[key].empty?
         end_row = nd
         until /#END_ROW:#{key.upcase.to_s}#/.match(end_row.text.to_s)
           garbage << end_row
           end_row = end_row.next
         end
-        return garbage + [end_row]
+        garbage << end_row
+        garbage.map(&:unlink)
       else
         rows = Array.new
         start_row = nd
@@ -105,40 +124,38 @@ module DocxTemplater
             case nd.text.to_s
             when /#BEGIN_ROW:([A-Z0-9_]+)#/
               new_key = $1.downcase.to_sym
-              garbage += expand_loop(nd, new_key, element)
+              expand_loop(nd, new_key, element)
             when /#END_ROW:([A-Z0-9_]+)#/
               garbage << nd
             else
               new_node = nd.dup
               nd.add_next_sibling(new_node)
               subst_content(new_node, element)
-              nd.unlink
+              garbage << nd
             end
           end
         end
-        return garbage.uniq
+        garbage.map(&:unlink)
       end
     end
 
     def parse_content(elements, data=@data)
-      garbage = Array.new
       elements.each do |nd|
         case nd.name
         when "tr"
           case nd.text.to_s
           when /#BEGIN_ROW:([A-Z0-9_]+)#/
             key = $1.downcase.to_sym
-            garbage += expand_loop(nd, key, data)
+            expand_loop(nd, key, data)
           else # it's a normal table row
-            garbage += parse_content(nd.elements, data)
+            parse_content(nd.elements, data)
           end
         when "t" # It's a leaf that contains data to replace
           subst_content(nd, data) 
         else # it's neither a leaf or a loop so let's process it
-          garbage += parse_content(nd.elements, data)
+          parse_content(nd.elements, data)
         end
       end
-      return garbage.uniq
     end
 
     def subst_content(nd, data)
