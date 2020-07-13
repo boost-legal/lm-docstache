a968ec803ab697203b3dc97f1dfc9bd3a786daf2
Jonathan Stevens <jmsgofish@gmail.com>
2020-07-13 16:01:23 -0700

    JS - inline blocks (WIP)

--- lib/lm_docstache/renderer.rb
+++ lib/lm_docstache/renderer.rb
@@ -22,8 +22,43 @@ module LMDocstache
         Block.find_all(name: block[1], elements: @content.elements, data: @data, inverted: inverted, condition: block[2])
       end
       found_blocks.each do |block|
-        expand_and_replace_block(block) if block.present?
+        if block.inline
+          handle_inline_conditional(block) if block.present?
+        else
+          expand_and_replace_block(block) if block.present?
+        end
       end
+    end
+
+    def handle_inline_conditional(block)
+      replace_conditional(block, "HEY!!")
+# binding.pry
+#       block.content_elements.css('w|t').each do |text_el|
+#         if !(results = text_el.text.scan(/\{\{([\w\.]+)\}\}/).flatten).empty?
+#           rendered_string = text_el.text
+#           results.each do |r|
+#             rendered_string.gsub!("{{#{r}}}", "boo")
+#           end
+#           text_el.content = rendered_string
+#         end
+#       end
+#       return elements
+
+      # case block.type
+      # when :conditional
+      #   case condition = @data.get(block.name, condition: block.condition)
+      #   when Array
+      #     condition = !condition.empty?
+      #   else
+      #     condition = !!condition
+      #   end
+      #   condition = !condition if block.inverted
+      #   unless condition
+      #     block.content_elements.each(&:unlink) # instead of unlinking, replace the text!
+      #   end
+      # when :loop
+      #   # TODO handle inline loops
+      # end
     end
 
     def expand_and_replace_block(block)
@@ -58,6 +93,39 @@ module LMDocstache
       end
       block.opening_element.unlink
       block.closing_element.unlink
+    end
+
+    def replace_conditional(block, data)
+      @content.css('w|t').each do |text_el|
+        start_tag = "##{block.name}#{block.condition}"
+        end_tag = "/#{block.name}"
+
+        rendered_string = text_el.text
+
+        if !(results = rendered_string.scan(/\{\{\#(.*?)\}\}/).flatten).empty?
+          results.each do |r|
+            rendered_string.sub!("{{##{r}}}", "")
+            rendered_string.sub!("{{/#{block.name}}}", "")
+          end
+        end
+
+        if !(results1 = rendered_string.scan(/\{\{\^(.*?)\}\}/).flatten).empty?
+          results1.each do |r|
+            rendered_string.sub!("{{^#{r}}}", "")
+            rendered_string.sub!("{{/#{block.name}}}", "")
+          end
+        end
+
+        text_el.content = rendered_string
+
+        # if !(results = text_el.text.scan(/\{\{([\w\.]+)\}\}/).flatten).empty?
+        #   rendered_string = text_el.text
+        #   results.each do |r|
+        #     rendered_string.gsub!("{{#{r}}}", data.get(r).to_s)
+        #   end
+        #   text_el.content = rendered_string
+        # end
+      end
     end
 
     def replace_tags(elements, data)
