ca8526db5e6ee71da9497996f8800399ad568084
Leandro Camargo <leandroico@gmail.com>
2021-02-12 16:00:13 -0300

    LC - Implement logic for inline conditional blocks replacement

--- lib/lm_docstache.rb
+++ lib/lm_docstache.rb
@@ -4,9 +4,9 @@ require "lm_docstache/version"
 require "lm_docstache/data_scope"
 require "lm_docstache/document"
 require "lm_docstache/block"
+require "lm_docstache/parser"
 require "lm_docstache/condition"
 require "lm_docstache/conditional_block"
-require "lm_docstache/parser"
 require "lm_docstache/renderer"
 
 module LMDocstache; end
