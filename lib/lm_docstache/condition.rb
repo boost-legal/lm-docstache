bb623b788a24b7b767f854a2cce42af059684c98
Leandro Camargo <leandroico@gmail.com>
2021-02-12 12:01:19 -0300

    LC - Implement `Condition` evaluator by adding `evaluate` method

--- lib/lm_docstache/condition.rb
+++ lib/lm_docstache/condition.rb
@@ -1,5 +1,9 @@
 module LMDocstache
   class Condition
+    InvalidOperator = Class.new(StandardError)
+
+    ALLOWED_OPERATORS = %w(== ~=).freeze
+
     attr_reader :left_term, :right_term, :operator, :negation, :original_match
 
     def initialize(left_term:, right_term:, operator:, negation: false, original_match: nil)
@@ -8,6 +12,15 @@ module LMDocstache
       @operator = operator
       @negation = negation
       @original_match = original_match
+
+      unless ALLOWED_OPERATORS.include?(operator)
+        raise InvalidOperator, "Operator '#{operator}' is invalid"
+      end
+    end
+
+    def evaluate(value)
+      result = value.to_s.send(operator, right_term)
+      negation ? !result : result
     end
   end
 end
