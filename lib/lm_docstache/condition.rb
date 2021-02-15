cb155139ca2ff0fe540c1b1fec89f47f44a06412
Leandro Camargo <leandroico@gmail.com>
2021-02-15 12:34:02 -0300

    LC - Ignore conditional value quotes when they are present

--- lib/lm_docstache/condition.rb
+++ lib/lm_docstache/condition.rb
@@ -3,12 +3,14 @@ module LMDocstache
     InvalidOperator = Class.new(StandardError)
 
     ALLOWED_OPERATORS = %w(== ~=).freeze
+    STARTING_QUOTES = %w(' " “)
+    ENDING_QUOTES = %w(' " ”)
 
     attr_reader :left_term, :right_term, :operator, :negation, :original_match
 
     def initialize(left_term:, right_term:, operator:, negation: false, original_match: nil)
       @left_term = left_term
-      @right_term = right_term
+      @right_term = remove_quotes(right_term)
       @operator = operator
       @negation = negation
       @original_match = original_match
@@ -21,6 +23,15 @@ module LMDocstache
     def truthy?(value)
       result = value.to_s.send(operator, right_term)
       negation ? !result : result
+    end
+
+    private
+
+    def remove_quotes(value)
+      start_position = STARTING_QUOTES.include?(value[0]) ? 1 : 0
+      end_position = ENDING_QUOTES.include?(value[-1]) ? -2 : -1
+
+      value[start_position..end_position]
     end
   end
 end
