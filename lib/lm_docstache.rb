154d539f8181173445a69eae47590d7bbd3521eb
Leandro Camargo <leandroico@gmail.com>
2021-02-15 12:49:02 -0300

    LC - Remove code that became disposable after the refactoring

--- lib/lm_docstache.rb
+++ lib/lm_docstache.rb
@@ -1,9 +1,7 @@
 require 'nokogiri'
 require 'zip'
 require "lm_docstache/version"
-require "lm_docstache/data_scope"
 require "lm_docstache/document"
-require "lm_docstache/block"
 require "lm_docstache/parser"
 require "lm_docstache/condition"
 require "lm_docstache/conditional_block"
