60adc4dfa458e5b0ba3a15f92326f8c6cde50d5d
Leandro Camargo <leandroico@gmail.com>
2021-06-03 18:40:23 -0300

    LC - Go back to using `index` + `delete_at` in `unusable_tags` method
    Unfortunately we can't simplify `unusable_tags` by using `Array#-`
    method like we were using because we need to remove the occurrences one
    at a time instead multiple ones like `Array#-` does. And we can't remove
    the tags that are `Regexp` neither.

--- lib/lm_docstache/document.rb
+++ lib/lm_docstache/document.rb
@@ -61,9 +61,9 @@ module LMDocstache
     end
 
     def unusable_tags
-      # We're also removing the reamining conditional block tags because they
-      # are not considered inside the `fix_errors` logic.
-      (tags - usable_tags).reject { |tag| tag.is_a?(Regexp) }
+      usable_tags.reduce(tags) do |broken_tags, usable_tag|
+        broken_tags.delete_at(broken_tags.index(usable_tag)) && broken_tags
+      end
     end
 
     def fix_errors
